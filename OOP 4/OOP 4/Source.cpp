1190e315b482bc87f3297bf7510320ae41170871
VANIS <0olistiko0@gmail.com>
2024-10-02 23:54:00 +0300

    Add files via upload

--- OOP 4/OOP 4/Source.cpp
+++ OOP 4/OOP 4/Source.cpp
@@ -4,20 +4,63 @@
 template <typename Type>
 
 class Matrix {
-
+public:
 	std::vector<std::vector<Type>> _matrix;
 	int _rows;
 	int _columns;
 
-public:
+
 	Matrix(int rows, int columns) : _rows(rows), _columns(columns), _matrix(rows, std::vector<Type>(columns)) {}
 
-	void matrixSetElement(Type element, int i, int j) {
-		_matrix[i][j] = element;
+	std::vector<Type>& operator [] (int i) {
+		return _matrix[i];
 	}
 
-	Type matrixGetElement(int i, int j) {
-		return _matrix[i][j];
+	Matrix<Type> operator + (Matrix<Type>& other) {
+		if (!(_rows == other._rows && _columns == other._columns)) {
+			std::cout << "Matrices have different sizes";
+			return Matrix<Type>(0, 0);
+		}
+
+		Matrix resultMatrix(_rows, other._columns);
+		for (int i = 0; i < _rows; i++) {
+			for (int j = 0; j < other._columns; j++) {
+				resultMatrix[i][j] = _matrix[i][j] + other[i][j];
+			}
+		}
+		return resultMatrix;
+	}
+
+	Matrix<Type> operator - (Matrix<Type>& other) {
+		if (!(_rows == other._rows && _columns == other._columns)) {
+			std::cout << "Matrices have different sizes";
+			return Matrix<Type>(0, 0);
+		}
+
+		Matrix resultMatrix(_rows, other._columns);
+		for (int i = 0; i < _rows; i++) {
+			for (int j = 0; j < other._columns; j++) {
+				resultMatrix[i][j] = _matrix[i][j] - other[i][j];
+			}
+		}
+		return resultMatrix;
+	}
+
+	Matrix<Type> operator * (Matrix<Type>& other) {
+		if (!(_rows == other._columns && _columns == other._rows)) {
+			std::cout << "Matrices have different sizes";
+			return Matrix<Type>(0, 0);
+		}
+
+		Matrix resultMatrix(_rows, other._columns);
+		for (int i = 0; i < _rows; i++) {
+			for (int j = 0; j < other._columns; j++) {
+				for (int k = 0; k < _columns; k++) {
+					resultMatrix[i][j] += _matrix[i][k] * other[k][j];
+				}
+			}
+		}
+		return resultMatrix;
 	}
 
 	void matrixPrint() {
@@ -27,63 +70,50 @@ public:
 			}
 			std::cout << "\n";
 		}
+		std::cout << "\n";
 	}
 
-	Matrix matrixTranspose() {
-		Matrix transposedMatrix(_columns, _rows);
-		for (int i = 0; i < _rows; i++) {
-			for (int j = 0; j < _columns; j++) {
-				transposedMatrix.matrixSetElement(_matrix[i][j], j, i);
+	Matrix<Type> matrixTranspose() {
+		Matrix<Type> transposedMatrix(_columns, _rows);
+		for (int i = 0; i < _columns; i++) {
+			for (int j = 0; j < _rows; j++) {
+				transposedMatrix[i][j] = _matrix[j][i];
 			}
 		}
 		return transposedMatrix;
 	}
-
-	Matrix matrixSum(Matrix A, Matrix B) {
-		Matrix resultMatrix(A._rows, A._columns);
-		for (int i = 0; i < A._rows; i++) {
-			for (int j = 0; j < A._columns; j++) {
-				resultMatrix.matrixSetElement(A.matrixGetElement(i, j) + B.matrixGetElement(i, j), i, j);
-			}
-		}
-		return resultMatrix;
-	}
-
-	Matrix matrixMultiply(Matrix A, Matrix B) {
-		Matrix resultMatrix(A._rows, B._columns);
-		for (int i = 0; i < A._rows; i++) {
-			for (int j = 0; j < B._columns; j++) {
-				int res = 0;
-				for (int k = 0; k < A._columns; k++) {
-					res += A.matrixGetElement(i, k) * B.matrixGetElement(k, j);
-				}
-				resultMatrix.matrixSetElement(res, i, j);
-			}
-		}
-		return resultMatrix;
-	}
 };
 
 int main() {
-	int rows = 3, columns = 4;
+	int rows = 2, columns = 3;
 
 	Matrix<int> A(rows, columns);
 	Matrix<int> B(rows, columns);
+	Matrix<int> C(rows, columns);
 	
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
-			B.matrixSetElement(i + j, i, j);
-			A.matrixSetElement((j - i) * j, i, j);
+			A[i][j] = (j - i) * j;
+			B[i][j] = i + j;
 		}
 	}
 
-	A.matrixSetElement(6, 1, 1);
-	B.matrixSetElement(8, 1, 1);
+	A[1][1] = 6;
+	B[1][1] = 8;
 
 	A.matrixPrint();
 	B.matrixPrint();
 
-	Matrix<int> C(3, 3);
-	C = C.matrixSum(A, B);
+	B = B.matrixTranspose();
+
+	C = A * B;
+	C.matrixPrint();
+
+	B = B.matrixTranspose();
+
+	C = A + B;
+	C.matrixPrint();
+
+	C = A - B;
 	C.matrixPrint();
 }
