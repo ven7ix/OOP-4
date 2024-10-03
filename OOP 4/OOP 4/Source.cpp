e7e889f935e6feec8327d6859a91e5f17401e49b
VANIS <0olistiko0@gmail.com>
2024-10-03 23:36:23 +0300

    Add files via upload

--- OOP 4/OOP 4/Source.cpp
+++ OOP 4/OOP 4/Source.cpp
@@ -1,14 +1,15 @@
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 
 template <typename Type>
 
 class Matrix {
-public:
 	std::vector<std::vector<Type>> _matrix;
 	int _rows;
 	int _columns;
 
+public:
 
 	Matrix(int rows, int columns) : _rows(rows), _columns(columns), _matrix(rows, std::vector<Type>(columns)) {}
 
@@ -16,10 +17,14 @@ public:
 		return _matrix[i];
 	}
 
-	Matrix<Type> operator + (Matrix<Type>& other) {
+	const std::vector<Type>& operator [] (int i) const {
+		return _matrix[i];
+	}
+
+	Matrix operator + (const Matrix& other) {
 		if (!(_rows == other._rows && _columns == other._columns)) {
 			std::cout << "Matrices have different sizes";
-			return Matrix<Type>(0, 0);
+			return Matrix(0, 0);
 		}
 
 		Matrix resultMatrix(_rows, other._columns);
@@ -31,12 +36,11 @@ public:
 		return resultMatrix;
 	}
 
-	Matrix<Type> operator - (Matrix<Type>& other) {
+	Matrix operator - (const Matrix& other) {
 		if (!(_rows == other._rows && _columns == other._columns)) {
 			std::cout << "Matrices have different sizes";
-			return Matrix<Type>(0, 0);
+			return Matrix(0, 0);
 		}
-
 		Matrix resultMatrix(_rows, other._columns);
 		for (int i = 0; i < _rows; i++) {
 			for (int j = 0; j < other._columns; j++) {
@@ -46,7 +50,7 @@ public:
 		return resultMatrix;
 	}
 
-	Matrix<Type> operator * (Matrix<Type>& other) {
+	Matrix operator * (const Matrix& other) {
 		if (!(_rows == other._columns && _columns == other._rows)) {
 			std::cout << "Matrices have different sizes";
 			return Matrix<Type>(0, 0);
@@ -73,8 +77,8 @@ public:
 		std::cout << "\n";
 	}
 
-	Matrix<Type> matrixTranspose() {
-		Matrix<Type> transposedMatrix(_columns, _rows);
+	Matrix matrixTranspose() {
+		Matrix transposedMatrix(_columns, _rows);
 		for (int i = 0; i < _columns; i++) {
 			for (int j = 0; j < _rows; j++) {
 				transposedMatrix[i][j] = _matrix[j][i];
@@ -105,15 +109,6 @@ int main() {
 	B.matrixPrint();
 
 	B = B.matrixTranspose();
-
 	C = A * B;
-	C.matrixPrint();
-
-	B = B.matrixTranspose();
-
-	C = A + B;
-	C.matrixPrint();
-
-	C = A - B;
 	C.matrixPrint();
 }
